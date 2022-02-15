d7cc6a405584a688c5df261685a242d4f9825e53
muit <muit@piperift.com>
2022-02-15 21:49:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# LLVM build environment for Rift
+
+This repository contains a reproducible LLVM environment build for [Rift](https://github.com/PipeRift/rift)
+
+
+## Prerequisites
+In order to build LLVM you will need a Cpp compiler
+
+## Setup
+
+To build, simply run `build.py` using python:
+```bash
+py build.py
+```
+
+## Differences to llvm-project
+Only kept clang, lld and llvm folders
---
+++ build.py
@@ -0,0 +1,23 @@
+import os
+
+root = os.getcwd()
+llvm_path = os.path.join(root, 'llvm')
+host_path = os.path.join(root, 'build')
+
+print(">> Configure LLVM")
+os.system('cmake -S "{0}" -B "{1}" -DLLVM_ENABLE_PROJECTS="lld;clang" \
+  -DLLVM_ENABLE_LIBXML2=OFF \
+  -DLLVM_INCLUDE_TESTS=OFF \
+  -DLLVM_INCLUDE_GO_TESTS=OFF \
+  -DLLVM_INCLUDE_EXAMPLES=OFF \
+  -DLLVM_INCLUDE_BENCHMARKS=OFF \
+  -DLLVM_ENABLE_BINDINGS=OFF \
+  -DLLVM_ENABLE_OCAMLDOC=OFF \
+  -DLLVM_ENABLE_Z3_SOLVER=OFF \
+  -DCLANG_BUILD_TOOLS=OFF \
+  -DCLANG_INCLUDE_DOCS=OFF \
+  -DLLVM_INCLUDE_DOCS=OFF \
+  -DCMAKE_BUILD_TYPE=Release'.format(llvm_path, host_path))
+
+print("\n>> Build LLVM")
+os.system('cmake --build "{0}" --config Release --target install'.format(host_path))
