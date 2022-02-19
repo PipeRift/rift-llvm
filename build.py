89ff7c22e8fcd4fa9be4106b517d6a6fb57f70f5
muit <muit@piperift.com>
2022-02-19 23:17:38 +0100

    Build in debug as well

--- build.py
+++ build.py
@@ -1,24 +1,46 @@
 import os
+import sys, getopt
 
-root = os.getcwd()
-llvm_path = os.path.join(root, 'llvm')
-host_path = os.path.join(root, 'build')
 
-print(">> Configure LLVM")
-os.system('cmake -S "{0}" -B "{1}" -DLLVM_ENABLE_PROJECTS="lld;clang" \
-  -DLLVM_ENABLE_LIBXML2=OFF \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -DLLVM_INCLUDE_GO_TESTS=OFF \
-  -DLLVM_INCLUDE_EXAMPLES=OFF \
-  -DLLVM_INCLUDE_BENCHMARKS=OFF \
-  -DLLVM_ENABLE_BINDINGS=OFF \
-  -DLLVM_ENABLE_OCAMLDOC=OFF \
-  -DLLVM_ENABLE_Z3_SOLVER=OFF \
-  -DLLVM_BUILD_TOOLS=OFF \
-  -DCLANG_BUILD_TOOLS=OFF \
-  -DCLANG_INCLUDE_DOCS=OFF \
-  -DLLVM_INCLUDE_DOCS=OFF \
-  -DCMAKE_BUILD_TYPE=Release'.format(llvm_path, host_path))
+def main(argv):
+  help = 'test.py -c <Debug|Release|MinSizeRel|RelWithDebInfo>'
+  try:
+    opts, args = getopt.getopt(argv,"hc:",["config="])
+  except getopt.GetoptError:
+    print(help)
+    sys.exit(2)
 
-print("\n>> Build LLVM")
-os.system('cmake --build "{0}" --config Release --target install'.format(host_path))
+  config = 'Release'
+  for opt, arg in opts:
+    if opt == '-h':
+        print(help)
+        sys.exit()
+    elif opt in ("-c", "--config"):
+        config = arg
+
+
+  root = os.getcwd()
+  llvm_path = os.path.join(root, 'llvm')
+  host_path = os.path.join(root, 'build')
+
+  print(">> Configure LLVM ({})".format(config))
+  os.system('cmake -S "{}" -B "{}" -DLLVM_ENABLE_PROJECTS="lld;clang" \
+    -DLLVM_ENABLE_LIBXML2=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_INCLUDE_GO_TESTS=OFF \
+    -DLLVM_INCLUDE_EXAMPLES=OFF \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF \
+    -DLLVM_ENABLE_BINDINGS=OFF \
+    -DLLVM_ENABLE_OCAMLDOC=OFF \
+    -DLLVM_ENABLE_Z3_SOLVER=OFF \
+    -DLLVM_BUILD_TOOLS=OFF \
+    -DCLANG_BUILD_TOOLS=OFF \
+    -DCLANG_INCLUDE_DOCS=OFF \
+    -DLLVM_INCLUDE_DOCS=OFF \
+    -DCMAKE_BUILD_TYPE={}'.format(llvm_path, host_path, config))
+
+  print("\n>> Build LLVM")
+  os.system('cmake --build "{}" --config {} --target install'.format(host_path, config))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
