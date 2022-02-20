01395925c7e2213207a94e1c3133080c8552661e
muit <muit@piperift.com>
2022-02-20 17:44:04 +0100

    Removed unneccessary debug build

--- build.py
+++ build.py
@@ -10,18 +10,19 @@ def main(argv):
     print(help)
     sys.exit(2)
 
+  root = os.getcwd()
+  llvm_path = os.path.join(root, 'llvm')
+  build_path = os.path.join(root, 'build')
   config = 'Release'
+
   for opt, arg in opts:
     if opt == '-h':
         print(help)
         sys.exit()
     elif opt in ("-c", "--config"):
         config = arg
-
-
-  root = os.getcwd()
-  llvm_path = os.path.join(root, 'llvm')
-  host_path = os.path.join(root, 'build')
+    elif opt in ("-b", "--build-path"):
+        build_path = arg
 
   print(">> Configure LLVM ({})".format(config))
   os.system('cmake -S "{}" -B "{}" -DLLVM_ENABLE_PROJECTS="lld;clang" \
@@ -37,10 +38,10 @@ def main(argv):
     -DCLANG_BUILD_TOOLS=OFF \
     -DCLANG_INCLUDE_DOCS=OFF \
     -DLLVM_INCLUDE_DOCS=OFF \
-    -DCMAKE_BUILD_TYPE={}'.format(llvm_path, host_path, config))
+    -DCMAKE_BUILD_TYPE={}'.format(llvm_path, build_path, config))
 
   print("\n>> Build LLVM")
-  os.system('cmake --build "{}" --config {} --target install'.format(host_path, config))
+  os.system('cmake --build "{}" --config {} --target install'.format(build_path, config))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
