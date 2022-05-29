04599d3e133015c5f9216d4ee84a218bcde68453
muit <muit@piperift.com>
2022-05-29 11:11:46 +0200

    Small cicd changes

--- build.py
+++ build.py
@@ -11,10 +11,11 @@ def main(argv):
   parser = argparse.ArgumentParser(description = "Build LLVM for Rift", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--config", "-c", default="Release", help="Configuration to build LLVM in. Debug, Release, MinSizeRel or RelWithDebInfo")
   parser.add_argument("--build", "-b", default="build", help="Path where to build LLVM")
+  parser.add_argument("--no-build", action='store_true', help="Should build be skipped?")
   parser.add_argument("--install", "-i", default=None, help="Path where to install build LLVM files. If none provided, install won't be run")
   parser.add_argument("--projects", default="clang,lld", help="List of LLVM projects to include on the build")
   parser.add_argument("--targets", default="X86,ARM,AArch64,RISCV", help="List of LLVM targets to include on the build. 'all' will build all targets")
-  parser.add_argument("--keep-build", action='store_true', help="Should build files be kept after LLVM is installed? Keeping build uses disk space but speedsup rebuilds of LLVM")
+  parser.add_argument("--clean-build", action='store_true', help="Should build files be cleaned after LLVM is build and/or installed? Keeping build uses disk space but speeds up rebuilds of LLVM")
   args = parser.parse_args()
 
   # Validate parameters
@@ -52,7 +53,8 @@ def main(argv):
 
 
   print("\n>> Build LLVM")
-  os.system('cmake --build "{}" --config {} --target install'.format(args.build, args.config))
+  if not args.no_build:
+    os.system('cmake --build "{}" --config {} --target install'.format(args.build, args.config))
 
   if args.install:
     print("\n>> Install LLVM")
@@ -61,7 +63,7 @@ def main(argv):
     args.install = os.path.join(args.install, args.config)
     os.system('cmake -DCMAKE_INSTALL_PREFIX={} -P {}/cmake_install.cmake'.format(args.install, args.build))
 
-  if not args.keep_build:
+  if args.clean_build:
     shutil.rmtree(args.build)
 
   print("Took {:.2f} seconds".format(time.time() - start_time))
